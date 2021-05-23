4e1e3868bb45e22f9017358f9dc643ec9ab7d7d1
이정민 <ljm@ijeongmin-ui-MacBookPro.local>
2021-05-23 18:23:19 +0900

    CREATE: 프로젝트 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'django-insecure-5xu5@h^ts(#-j%8xf^es7v(bc!rdn%ugykzro7#et_3fx#@@o@'
+
+
+DATABASES = {
+'default' : {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'westarbucks',
+'USER': 'root',
+'PASSWORD': '123',
+'HOST': 'localhost',
+'PORT': '3306',
+}
+}
